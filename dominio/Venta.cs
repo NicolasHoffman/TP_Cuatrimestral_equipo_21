37e129292342def8f78fd27edb700fb321599def
nhoffman <nicolas.hoffman@alumnos.frgp.utn.edu.ar>
2024-06-29 20:23:46 -0300

    venta

---
+++ dominio/Venta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        public DateTime FechaVenta { get; set; }
+        public int IdCliente { get; set; }
+        public int IdVendedor { get; set; }
+        public int IdFormaDePago { get; set; }
+        public decimal ImporteTotal { get; set; }
+
+    }
+}
