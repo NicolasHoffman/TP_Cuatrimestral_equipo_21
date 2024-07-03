42aed9c3fdbad5ae6151112e6331a1ca5eb08e9f
nhoffman <nicolas.hoffman@alumnos.frgp.utn.edu.ar>
2024-07-02 23:12:17 -0300

    Pedido

---
+++ dominio/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public Venta Venta { get; set; }
+        public EstadoPedido EstadoPedido { get; set; }
+        public bool EstadoP { get; set; }
+    }
+}
---
+++ negocio/PedidoNegocio.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class PedidoNegocio
+    {
+        public List<Pedido> listar()
+        {
+            List<Pedido> lista = new List<Pedido>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta(@"SELECT P.Id, P.EstadoP,V.Id AS VentaId, V.FechaVenta, V.IdCliente, V.IdVendedor, V.IdFormaDePago, V.ImporteTotal, EP.Id AS EstadoPedidoId, EP.Descripcion AS EstadoPedidoDescripcion FROM PEDIDO P INNER JOIN VENTA V ON V.Id = P.IdVenta INNER JOIN ESTADO_PEDIDO EP ON EP.Id = P.IdEstadoPedido");
+                datos.ejecturaLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Pedido aux = new Pedido();
+
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.EstadoP = (bool)datos.Lector["EstadoP"];
+
+                    aux.Venta = new Venta();
+                    aux.Venta.Id = (int)datos.Lector["VentaId"];
+                    aux.Venta.FechaVenta = (DateTime)datos.Lector["FechaVenta"];
+                    aux.Venta.IdCliente = (int)datos.Lector["IdCliente"];
+                    aux.Venta.IdVendedor = (int)datos.Lector["IdVendedor"];
+                    aux.Venta.IdFormaDePago = (int)datos.Lector["IdFormaDePago"];
+                    aux.Venta.ImporteTotal = (decimal)datos.Lector["ImporteTotal"];
+
+                    aux.EstadoPedido = new EstadoPedido();
+                    aux.EstadoPedido.Id = (int)datos.Lector["EstadoPedidoId"];
+                    aux.EstadoPedido.Descripcion = (string)datos.Lector["EstadoPedidoDescripcion"];
+
+                    lista.Add(aux);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+            return lista;
+        }
+    }
+}
